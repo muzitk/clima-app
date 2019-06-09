52bb9561ce1844f0fdc298ad3beb6cfbe84b6675
muzitk <msebhatlab1@gmail.com>
2019-06-09 12:42:21 -0700

    created all components needed for this application

---
+++ src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Titles extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1> Check Weather </h1>
+        <p>Find out what the weather is like at any given area </p>
+      </div>
+    )
+  }
+}
+export default Titles;
