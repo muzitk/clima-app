a3c93f66f3d649f9e2beebf60037dbf8b9988cbb
muzitk <msebhatlab1@gmail.com>
2019-07-10 14:11:20 -0700

    added styling and README

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,11 +4,19 @@ class Form extends React.Component{
   render(){
     return(
       <form onSubmit = {this.props.getWeather}>
-        <input type="text" name="city" placeholder="City" />
-        <input type="text" name="country" placeholder="Country" />
-        <button>Check Weather</button>
+        <label>       
+           <input type="text" name="city" placeholder="City" />
+        </label>
+        <label>   
+         <input type="text" name="country" placeholder="Country" />
+        </label>
+        <label>        
+          <button className="button">Check Weather</button>
+        </label>
       </form>
     )
   }
 }
 export default Form;
+
+
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,8 +4,8 @@ class Titles extends React.Component{
   render(){
     return(
       <div className = "titles">
-        <h1> Check Weather </h1>
-        <p>Find out what the weather is like at any given area </p>
+        <h1 className="header"> Check Weather </h1>
+        <p className = "subheader">Find out what the weather is like at any given area </p>
       </div>
     )
   }
